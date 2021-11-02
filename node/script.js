066ff0583470a73e24cf53fb67f14108a8f27b24
Johannes Palojärvi laptop <johannes.palojarvi@gmail.com>
2021-11-02 15:09:49 +0200

    reviewed week 1 exercises

--- node/script.js
+++ node/script.js
@@ -1,3 +1,10 @@
 'use strict';
+
+const _ = require('lodash');
+
+const foobar = 'Foo Bar';
+const camelized = _.camelCase(foobar);
+
 console.log(
     'This is JavaScript');
+console.log(camelized);
